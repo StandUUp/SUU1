e9fee4f7f419f86ad6647d329ee2e51d3339038b
gloye <dbzbook@hotmail.com>
2016-03-23 22:14:41 +0800

    add a note

--- sunsx/js/task14.js
+++ sunsx/js/task14.js
@@ -12,7 +12,7 @@ var aqiData = [
 
 (function () {
     /*
-    任务要求：
+     任务要求：
      在注释下方编写代码
      遍历读取aqiData中各个城市的数据
      将空气质量指数大于60的城市显示到aqi-list的列表中
@@ -24,26 +24,28 @@ var aqiData = [
      */
     var list = document.getElementById("aqi-list");
     //比较方法
-    function compare(num1,num2){
-        return num1[1]-num2[1];
+    function compare(num1, num2) {
+        return num1[1] - num2[1];
     }
+
     //插入结果函数
-    function insert(rank,data){
+    function insert(rank, data) {
         var item = document.createElement("li");
-        item.innerHTML='第'+rank+'名'+data;
+        item.innerHTML = '第' + rank + '名' + data;
         list.appendChild(item);
     }
+
     //排序
     var sortArr = aqiData.sort(compare);
     var relArr = [];
     //筛选
-    for(var i in sortArr){
-        if(sortArr[i][1]>60)
-        relArr.push(sortArr[i]);
+    for (var i in sortArr) {
+        if (sortArr[i][1] > 60)
+            relArr.push(sortArr[i]);
     }
     //输出
-    for(var i in relArr){
-        var num = parseInt(i)+1;
-        insert(num,relArr[i]);
+    for (var i in relArr) {
+        var num = parseInt(i) + 1;
+        insert(num, relArr[i]);
     }
 })();
---
+++ sunsx/note.md
@@ -0,0 +1,3 @@
+#review别人代码，战略性mark
+- task3小伙伴交给我**&lt;figure&gt;**标签与**&lt;figcaption&gt;**标签
+- task14有的小伙伴使用了filter()方法
